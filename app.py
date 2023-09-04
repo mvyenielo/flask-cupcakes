97577fd3e072810dc782f8e5548f7e1059bae7fb
Daniel Zych <danjzych@gmail.com>
2023-09-04 14:26:39 -0500

    Completed first set of route sfor Cupcake API

--- app.py
+++ app.py
@@ -10,5 +10,41 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 
+
 @app.get("/api/cupcakes")
 def get_all_cupcakes():
+    """Return JSON for all cupcakes - ..."""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [cupcake.serialize() for cupcake in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.get('/api/cupcakes/<int:cupcake_id>')
+def get_cupcake(cupcake_id):
+    """Return JSON for an individual cupcake."""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+
+@app.post('/api/cupcakes')
+def create_cupcake():
+    """Create a cupcake in DB and returns JSON for cupcake."""
+
+    flavor = request.json['flavor']
+    size = request.json['size']
+    rating = request.json['rating']
+    image_url = request.json['image_url']
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image_url=image_url)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return ((jsonify(cupcake=serialized)), 201)
