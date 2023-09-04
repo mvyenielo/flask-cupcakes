ab81dcda590d1a5749544207bddb825898bcf632
Megan Vyenielo <mvyenielo58@gmail.com>
2023-09-04 12:01:58 -0700

    built model for cupcakes db and serialize function

--- models.py
+++ models.py
@@ -1,1 +1,55 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Cupcake(db.Model):
+    """Cupcake model"""
+
+    __tablename__="cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    flavor = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    size = db.Column(
+        db.String(15),
+        nullable=False
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(500),
+        nullable=False,
+        default="https://tinyurl.com/demo-cupcake"
+    )
+
+    def serialize(self):
+        """Serialize to dictionary."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image_url": self.image_url
+        }
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
